7c48470e2d2bbbf4ebdcc30193caa4fed9bdb6a8
Valerie Roussel <59619956+ValerieRoussel@users.noreply.github.com>
2022-02-05 19:05:30 -0500

    added TEMP, TO BE REPLACED intro screen

---
+++ Assets/Scripts/UI_Elements/TEMP_IntroScreen.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TEMP_IntroScreen : MonoBehaviour
+{
+    public RawImage img;
+
+    public float introTime;
+
+    private float currTime;
+    private bool renderingImg;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderingImg = true;
+        currTime = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (renderingImg)
+        {
+            currTime += Time.deltaTime;
+            float alpha = Mathf.Clamp(introTime - currTime, 0.0f, 1.0f);
+            
+            var color = img.color;
+            color.a = alpha;
+            img.color = color;
+
+            if (alpha == 0.0f)
+            {
+                renderingImg = false;
+                img.gameObject.SetActive(false);
+            }
+        }
+    }
+}
