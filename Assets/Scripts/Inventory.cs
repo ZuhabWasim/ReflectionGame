d104e64846c4f839d9145c9f960b5198d5aa3a1b
flryang <florayangmeilin@gmail.com>
2022-02-12 13:55:24 -0500

    Inventory UI working

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -12,11 +12,12 @@ public enum ItemPickupResult
 // Singleton Class, should not be instantiated!! Use Inventory.GetInstance instead
 public class Inventory
 {
-    public int inventorySize = 5;
-    private List<PickupItem> m_items = new List<PickupItem>();
+    public int inventorySize = 8;
+    private static PickupItem[] empty = {null, null, null, null, null, null, null, null};
+    private List<PickupItem> m_items = new List<PickupItem>(empty);
     private static Inventory m_instance = new Inventory();
-    private int cursor = 0;
-    
+    private int cursor = 2;
+    private InventoryDisplay inventoryDisplay = GameObject.Find("UI_Canvas").GetComponent<InventoryDisplay>();
     public static ref Inventory GetInstance()
     {
         return ref m_instance;
@@ -29,24 +30,47 @@ public class Inventory
             return ItemPickupResult.FAIL_ERROR;
         }
 
-        if ( m_items.Count == inventorySize )
-        {
-            return ItemPickupResult.FAIL_INVENTORY_FULL;
+        for (int i = 0; i < inventorySize; i++){
+            if (m_items[i] == null){
+                m_items[i] = item;
+                item.OnPickup();
+                inventoryDisplay.addItem(item, i);
+                return ItemPickupResult.SUCCESS;
+            }
         }
-
-        m_items.Add( item );
-        item.OnPickup();
-        return ItemPickupResult.SUCCESS;
+        return ItemPickupResult.FAIL_INVENTORY_FULL;
     }
 
     public void MoveCursor( int newPos )
     {
-        if ( newPos < m_items.Count )
+        if ( newPos < 0) {
+            cursor = inventorySize + newPos;
+        } else if ( newPos < inventorySize )
         {
             cursor = newPos;
+        } else {
+            cursor = newPos - inventorySize;
         }
     }
-
+    public void openInventory()
+    {
+        inventoryDisplay.openInventory();
+        if (m_items[cursor] != null){
+            PickupItem item = m_items[cursor];
+            inventoryDisplay.showItemName(item.itemName);
+        } else {
+            inventoryDisplay.hideItemName();
+        }
+    }    
+    public void closeInventory()
+    {
+        inventoryDisplay.closeInventory();
+    }
+    public void spinInventory(int spin)
+    {
+        MoveCursor( cursor + spin );
+        inventoryDisplay.spinInventory(spin);
+    }
     public int GetCursor()
     {
         return cursor;
@@ -58,15 +82,10 @@ public class Inventory
         {
             PickupItem item = m_items[ cursor ];
             item.OnDrop( dropPosition );
-            m_items.RemoveAt( cursor );
-            if ( cursor > 0 )
-            {
-                cursor--;
-            }
-
+            m_items[cursor] = null;
+            inventoryDisplay.dropItem(cursor);
             return true;
         }
-
         Debug.Log( "Tried to drop item from a slot that doesn't have a valid item" );
         return false;
     }
