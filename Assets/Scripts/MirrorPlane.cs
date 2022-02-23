388588fd7a7e7f0ce3053a4ebb1a97cc7fda2998
Dennis Wang <mian.wang@mail.utoronto.ca>
2022-02-22 22:27:56 -0700

    Mirror refactor alpha (#32)
    * Refactor player public variable in MirrorCameraPosition to a private variable in MirrorConnector
    
    * Move MirrorCameraPosition.cs to Scripts/
    
    * Refactor MirrorConnector to use MirrorPlane rather than MirrorCameraPosition
    
    * Refactor MirrorCameraPosition variables
    
    * Programmatically set textures/materials for mirrors
    
    * Fix issues with Start() in MirrorCameraPosition and MirrorPlane

--- Assets/Scripts/MirrorPlane.cs
+++ Assets/Scripts/MirrorPlane.cs
@@ -8,22 +8,34 @@ public class MirrorPlane : MonoBehaviour
     public bool isDirty = false;
     public Material dirtyMirrorMaterial; // Dirty texture to use for dirty mirrors
 
-    public Material mirrorMaterial; // the original mirror material (i.e. the reflective one)
-    private Renderer mirrorRenderer; // TODO(dennis): remove this because i hate it
+    private Renderer m_mirrorRenderer;
+    private Material m_mirrorMaterial; // the original mirror material (i.e. the reflective one)
+
+    // Child components of the mirror
+    private MirrorCameraPosition m_mirrorCameraPosition;
 
     // Start is called before the first frame update
     void Start()
     {
-        mirrorRenderer = GetComponent<Renderer>();
-        mirrorMaterial = mirrorRenderer.material;
+        m_mirrorCameraPosition = GetComponentInChildren<MirrorCameraPosition>();
+        if (m_mirrorCameraPosition == null)
+        {
+            Debug.LogError("Cannot find MirrorCameraPosition in Mirror " + this.name);
+        }
+
+        if (m_mirrorRenderer == null || m_mirrorMaterial == null)
+        {
+            SetupRendererAndMaterial();
+        }
+
         if (isDirty)
         {
             // Change the renderer's material to the texture
-            mirrorRenderer.material = dirtyMirrorMaterial;
+            m_mirrorRenderer.material = dirtyMirrorMaterial;
         }
         else
         {
-            mirrorRenderer.material = mirrorMaterial;
+            m_mirrorRenderer.material = m_mirrorMaterial;
         }
     }
 
@@ -38,8 +50,51 @@ public class MirrorPlane : MonoBehaviour
         if (isDirty)
         {
             isDirty = false;
-            mirrorRenderer.material = mirrorMaterial;
+            m_mirrorRenderer.material = m_mirrorMaterial;
             // mirrorMaterial.Lerp( dirtyMirrorMaterial, mirrorMaterial, 0 ); TODO: Fix this, doesn't work atm
         }
+    }
+
+    public void ReflectOverMirror(Transform player, Transform playerCamera)
+    {
+        m_mirrorCameraPosition.ReflectOverMirror(player, playerCamera);
+    }
+
+    public void SetOppositeCameraPosition(Transform player, Transform playerCamera, Transform otherMirrorPlane)
+    {
+        m_mirrorCameraPosition.SetOppositeCameraPosition(player, playerCamera, otherMirrorPlane);
+    }
+
+    // Sets what texture the camera will render to
+    public void SetCameraRenderTexture(RenderTexture texture)
+    {
+        m_mirrorCameraPosition = GetComponentInChildren<MirrorCameraPosition>();
+        if (m_mirrorCameraPosition == null)
+        {
+            Debug.LogError("Cannot find MirrorCameraPosition in Mirror " + this.name);
+        }
+        else
+        {
+            m_mirrorCameraPosition.SetMirrorRenderTexture(texture);
+        }   
+    }
+
+    // Sets what texture the mirror displays. Used in conjunction with past mirrors
+    public void SetMirrorDisplayTexture(RenderTexture texture)
+    {
+        if (m_mirrorMaterial == null)
+        {
+            SetupRendererAndMaterial();
+        }
+
+        // Don't need to make a new material here
+        m_mirrorMaterial.SetTexture("_MainTex", texture);
+    }
+
+    // Sets up the m_mirrorRenderer and m_mirrorMaterial. Used since initialization order is not defined.
+    private void SetupRendererAndMaterial()
+    {
+        m_mirrorRenderer = GetComponent<Renderer>();
+        m_mirrorMaterial = m_mirrorRenderer.material;
     }
 }
