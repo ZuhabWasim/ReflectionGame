abd2490ce0ee546f10e0024b4d5535c187242ffe
Valerie Roussel <valerie.a.roussel@gmail.com>
2022-02-26 22:11:29 -0500

    added voicelines and music box script

---
+++ Assets/Scripts/Section4/MusicBox.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicBox : InteractableAbstract
+{
+    public override void OnUserInteract()
+    {
+        //TODO this
+    }
+
+}
