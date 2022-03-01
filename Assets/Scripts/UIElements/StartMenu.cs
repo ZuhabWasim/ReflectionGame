f03d5286651d5a35a35c072352169a47675484f1
Flryang <54019127+Flryang@users.noreply.github.com>
2022-03-01 03:46:11 -0500

    Title Screen (#35)
    * create startmenu
    
    * working start menu

---
+++ Assets/Scripts/UIElements/StartMenu.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+
+    public void onClickStart() {
+        SceneManager.LoadScene(1);
+    }
+    public void onClickQuit() {
+        Application.Quit();
+    }
+}
