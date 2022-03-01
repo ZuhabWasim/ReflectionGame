c194cd9e688676ca1d61c2696d0b7f28cd8a9527
Dennis Wang <mian.wang@mail.utoronto.ca>
2022-02-28 20:25:10 -0700

    Mirror teleport (#34)
    * Update .gitignore
    
    * Add teleporting to mirrors
    
    * Use event system for mirror teleporter
    
    * Added Present Mom Closet Layout
    
    Need to move it into position
    
    * Added rails for past mirror B
    
    * Created Room prefabs for the Main Room and Mom's closet
    
    * Added Dads closet and made All three rooms prefabs
    
    * Update .gitignore
    
    * Add teleporting to mirrors
    
    * Use event system for mirror teleporter
    
    * Change mirror teleport zone to use a rectangle
    
    Co-authored-by: Zuhab Wasim <z-wasim-786@live.com>

---
+++ Assets/Scripts/MirrorManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Any state mirror management should happen here.
+public class MirrorManager : MonoBehaviour
+{
+    public MirrorConnector section1Mirror;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool canTP = false;
+
+        canTP |= section1Mirror.CanTeleport();
+
+        GlobalState.SetVar<bool>(Globals.Vars.CAN_TELEPORT, canTP);
+    }
+}
