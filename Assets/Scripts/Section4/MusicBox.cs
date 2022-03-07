f7bdce959fefe3b7c6a738fcb6e7adad20051584
Zuhab Wasim <z-wasim-786@live.com>
2022-03-07 15:13:48 -0500

    Added soem voiceliens and new event for Millie Key

--- Assets/Scripts/Section4/MusicBox.cs
+++ Assets/Scripts/Section4/MusicBox.cs
@@ -4,9 +4,43 @@ using UnityEngine;
 
 public class MusicBox : InteractableAbstract
 {
-    protected override void OnUserInteract()
+    
+    private const string MUSICBOX_AUDIO_SOURCE = "MusicBoxAudioSource";
+    private Inventory m_inventory;
+    public AudioClip millieKeyVoiceline;
+    public AudioClip motherKeyVoiceline;
+    public AudioClip fatherKeyVoiceline;
+    
+    protected override void OnStart()
     {
-        //TODO this
+        EventManager.Sub(Globals.Events.MILLIE_KEY_INTERACT, MillieKeyInteract);
+        
+        m_inventory = Inventory.GetInstance();
+        // AudioPlayer.RegisterAudioPlayer(MUSICBOX_AUDIO_SOURCE, GetComponent<AudioSource>());
+        
+        desiredItem = Globals.UIStrings.MUSICBOXMILLIE_ITEM;
+    }
+
+    protected override void OnUserInteract()
+    {   
+        AudioPlayer.Play( voiceLine, Globals.Tags.MAIN_SOURCE );
+    }
+
+    protected override void OnUseItem()
+    {
+        Debug.Log("Initially interacting with the Music Box with Millie's key");
+        HandleInteract();
+    }
+
+    void HandleInteract()
+    {
+        MillieKeyInteract();
+    }
+
+    void MillieKeyInteract()
+    {
+        Debug.Log("MillieKeyInteract");
+        AudioPlayer.Play( millieKeyVoiceline, Globals.Tags.MAIN_SOURCE );
     }
 
 }
