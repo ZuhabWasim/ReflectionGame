54371b551088023d7b4b75bb2200a93cdfcc4375
Lakshya2610 <lakshyagupta26@live.com>
2022-03-17 02:16:13 -0400

    Added more color filtering options

--- Assets/Scripts/Section3/DadPuzzle2/ColorFilter.cs
+++ Assets/Scripts/Section3/DadPuzzle2/ColorFilter.cs
@@ -2,27 +2,72 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum ColorFilterMode
+{
+	SUBTRACTIVE = 0,
+	ADDITIVE,
+	INVERT_SUBTRACTIVE
+}
+
 public class ColorFilter : MonoBehaviour
 {
-    public Light outgoingLight;
-    public Color color;
-    [Range(0.0f, 1.0f)]
-    public float subtractiveFactor = 0.9f;
-    
-    public Color FilterColor( Color input )
-    {
-        EnableLight();
-        outgoingLight.color = input - ( subtractiveFactor * color );
-        return outgoingLight.color;
-    }
+	public Light outgoingLight;
+	public Color color;
+	[Range( 0.0f, 1.0f )]
+	public float factor = 0.9f;
+	public ColorFilterMode filterMode = ColorFilterMode.SUBTRACTIVE;
 
-    public void DisableLight()
-    {
-        outgoingLight.enabled = false;
-    }
+	public Color FilterColor( Color input )
+	{
+		EnableLight();
+		switch ( filterMode )
+		{
+			case ColorFilterMode.SUBTRACTIVE:
+				outgoingLight.color = FSubtractive( input );
+				break;
+			case ColorFilterMode.ADDITIVE:
+				outgoingLight.color = FAdditive( input );
+				break;
+			case ColorFilterMode.INVERT_SUBTRACTIVE:
+				outgoingLight.color = FInvertedSubtractive( input );
+				break;
+		}
 
-    public void EnableLight()
-    {
-        outgoingLight.enabled = true;
-    }
+		return outgoingLight.color;
+	}
+
+	private Color FSubtractive( Color input )
+	{
+		return input - ( factor * color );
+	}
+
+	private Color FInvertedSubtractive( Color input )
+	{
+		input = new Color( 1, 1, 1, 1 ) - input;
+		return Abs( input - ( factor * color ) );
+	}
+
+	private Color FAdditive( Color input )
+	{
+		return input + ( factor * color );
+	}
+
+	private Color Abs( Color c )
+	{
+		c.r = Mathf.Abs( c.r );
+		c.g = Mathf.Abs( c.g );
+		c.b = Mathf.Abs( c.b );
+		c.a = Mathf.Abs( c.a );
+		return c;
+	}
+
+	public void DisableLight()
+	{
+		outgoingLight.enabled = false;
+	}
+
+	public void EnableLight()
+	{
+		outgoingLight.enabled = true;
+	}
 }
