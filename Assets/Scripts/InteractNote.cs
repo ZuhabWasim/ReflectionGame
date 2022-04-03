d9c12297d758707d62518dc7201bc2faf0a3ed2a
Zuhab Wasim <z-wasim-786@live.com>
2022-04-03 04:19:47 -0400

    Generalized InteractNote.cs to be used inplace of all subscripts

--- Assets/Scripts/InteractNote.cs
+++ Assets/Scripts/InteractNote.cs
@@ -1,18 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class InteractNote : InteractableAbstract
 {
-
-	public int id;
-	public Texture noteText;
 	public static List<InteractNote> journal = new List<InteractNote>();
 	public static int pointer = 0;
+	
 	public string title;
-	public string onReadFireEvent;
+	public Texture noteText;
+	
+	public AudioClip letterAudio;
+	public AudioClip responseAudio;
 
-	protected override void OnUserInteract()
+	public string onReadFireEvent = "";
+	
+	public override void OnUseItemUnfiltered()
+	{
+		AddNote();
+		
+		AudioPlayer.Play( Globals.AudioFiles.General.PAPER_UNRAVEL, Globals.Tags.MAIN_SOURCE );
+		AudioPlayer.Play( letterAudio, Globals.Tags.DIALOGUE_SOURCE );
+		AudioPlayer.Play( responseAudio, Globals.Tags.DIALOGUE_SOURCE, false );
+		if (onReadFireEvent != string.Empty)
+		{
+			EventManager.Fire( onReadFireEvent );
+		}
+		EventManager.Fire( InputManager.GetKeyDownEventName( Keybinds.TAB_KEY ) );
+	}
+
+	public void AddNote()
 	{
 		if ( pointer == 0 )
 		{
@@ -21,7 +39,5 @@ public class InteractNote : InteractableAbstract
 		journal.Add(this);
 		this.gameObject.SetActive( false );
 		pointer += 1;
-		AudioPlayer.Play( Globals.AudioFiles.General.PAPER_UNRAVEL, Globals.Tags.MAIN_SOURCE );
-		EventManager.Fire( InputManager.GetKeyDownEventName( Keybinds.TAB_KEY ) );
 	}
 }
